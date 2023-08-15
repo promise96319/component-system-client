66c9387486e79ceabed28a1ab05117da5a414376
Guanghui Qin <1248975357@qq.com>
2023-08-15 16:55:24 +0800

    feat: 添加 backtop

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,10 +1,12 @@
 import { Layout, Header } from '@/components';
 import { AdminSidebar } from '@/components/admin';
+import { BackTop } from '@/components/arco';
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   return (
     <Layout header={<Header></Header>} sidebar={<AdminSidebar></AdminSidebar>}>
       {children}
+      <BackTop></BackTop>
     </Layout>
   );
 }
---
+++ src/components/arco/index.ts
@@ -0,0 +1,3 @@
+'use client';
+
+export * from '@arco-design/web-react';
