cefc487caf02273b877f083b068c390f1b4b77cd
Guanghui Qin <1248975357@qq.com>
2023-08-15 15:45:03 +0800

    feat: 需求搜索

--- src/app/admin/demand/page.tsx
+++ src/app/admin/demand/page.tsx
@@ -1,24 +1,27 @@
 'use client';
 
-import { Empty, Input } from '@arco-design/web-react';
+import { Empty, Input, Select, Space } from '@arco-design/web-react';
 import { IconSearch } from '@arco-design/web-react/icon';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { AdminContainer } from '@/components/admin/admin-container/admin-container';
 import { DemandList } from '@/components/demand';
 import { useMajorVersionId } from '@/hooks/use-major-version-id';
+import { DemandStatus } from '@/services/common';
 import { useDemands } from '@/services/demand';
 
 export default function Demand() {
   const [majorVersionId] = useMajorVersionId();
   const [content, setContent] = useState('');
+  const [status, setStatus] = useState();
   const {
     data: demands,
     error,
     mutate: updateDemands
   } = useDemands({
     majorVersionId,
-    content
+    content,
+    status
   });
 
   const searchParams = useSearchParams();
@@ -34,7 +37,7 @@ export default function Demand() {
 
   useEffect(() => {
     updateDemands();
-  }, [majorVersionId, content, updateDemands]);
+  }, [majorVersionId, content, status, updateDemands]);
 
   if (error) {
     return null;
@@ -42,12 +45,31 @@ export default function Demand() {
 
   return (
     <AdminContainer title="需求列表">
-      <Input
-        prefix={<IconSearch></IconSearch>}
-        onChange={(content) => {
-          setContent(content);
-        }}
-      ></Input>
+      <Space>
+        <Select
+          defaultValue="ALL"
+          prefix="需求状态"
+          options={[
+            { label: '全部', value: 'ALL' },
+            { label: '已解决', value: DemandStatus.CLOSED },
+            { label: '待解决', value: DemandStatus.OPENED }
+          ]}
+          onChange={(value) => {
+            if (value === 'ALL') {
+              setStatus(undefined);
+            } else {
+              setStatus(value);
+            }
+          }}
+        ></Select>
+        <Input
+          prefix={<IconSearch></IconSearch>}
+          placeholder="搜索需求内容"
+          onChange={(content) => {
+            setContent(content);
+          }}
+        ></Input>
+      </Space>
 
       {demands.length === 0 ? (
         <Empty style={{ marginTop: 160 }}></Empty>
